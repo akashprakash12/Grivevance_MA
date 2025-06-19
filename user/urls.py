0b53dd23982bb96c9fc69c923f684edead956f05
akashprakash12 <akashprakash7032@gmail.com>
2025-06-18 23:52:22 -0700

    user modified

--- user/urls.py
+++ user/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'public_user'  # <--- VERY IMPORTANT
 
 urlpatterns = [
-    path('create/', views.create_public_user, name='create_public_user'),
+    path('create/', views.create_public_user, name='user_dashbard'),
     path('view/', views.view_public_users, name='view_public_users'),
     path('update/<str:username>/', views.update_public_user, name='update_public_user'),
     path('delete/<str:username>/', views.delete_public_user, name='delete_public_user'),
